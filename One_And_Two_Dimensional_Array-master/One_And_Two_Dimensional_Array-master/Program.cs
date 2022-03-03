22cb81e6089e1ffb5b9c5f8622d6fefaf9faa549
prithviv35 <100734967+prithviv35@users.noreply.github.com>
2022-03-03 12:25:36 +0530

    Add files via upload

---
+++ One_And_Two_Dimensional_Array-master/One_And_Two_Dimensional_Array-master/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One_And_Two_Dimensional_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring single dimensional array
+            string[] Books = new string[5];
+            Books[0] = "C#";
+            Books[1] = "Java";
+            Books[2] = "VB.NET";
+            Books[3] = "C++";
+            Books[4] = "C";
+
+            Console.WriteLine("All the element of Books array is:\n");
+
+            //Formatting Output
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("{1}. {0}\t", Books[i-1],i);
+            }
+            Array.Sort(Books);
+            foreach (String s in Books)
+            {
+                Console.WriteLine(s);  
+            }
+
+            //initializing 2D array
+            int[,] numbers = { { 2, 3 }, { 4, 5 } };
+
+            // access first element from the first row
+            Console.WriteLine("Element at index [0, 0] : " + numbers[0, 0]);
+
+            // access first element from second row
+            Console.WriteLine("Element at index [1, 0] : " + numbers[1, 0]);
+
+            Console.ReadLine();
+
+        }
+    }
+}
